888cf1273f1a2491de602019d0775c67008fce67
frogrammer9 <oskar@meler.pl>
2024-05-21 23:27:24 +0200

    fixed Key enums

--- Xenon/src/core/input/input.hpp
+++ Xenon/src/core/input/input.hpp
@@ -14,13 +14,13 @@ public:
 	Input& operator=(const Input&&) = delete;
 
 	enum Key {
-		PageUp, PageDown, Pause, Np1, Np2, Np3, Np4, Np5, Np6, Np7, Np8, Np9, Np0,
+		Np1, Np2, Np3, Np4, Np5, Np6, Np7, Np8, Np9, Np0,
 		Enter, Shift, Control, Alt, Caps_Lock, Tab, Esc, Windows, Command, Super, Alt_Gr, Right_Control, Right_Shift,
 		Up, Down, Left, Right,
-		F1, F2, F3, F4, F5, Space, F7, F8, F9, F10, F11, F12, F13, 
+		F1, F2, F3, F4, F5, Space, F7, F8, F9, F10, F11, F12, Apostrophe,
 		Insert, Delete, Home, End,
 		Comma, Dash, Period, Slash, Nr0, Nr1, Nr2, Nr3, Nr4, Nr5, Nr6, Nr7, Nr8, Nr9, F6, Semicolon,
-		Apostrophe, Equals,
+		Equals,  F13, PageUp, PageDown, Pause, 
 		A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, Left_Braket, Backslash, Right_Braket,
 		Np_Devide, Np_Multiply, Np_Minus, Np_Add, Np_Enter,
 		Back_Quote, F14, F15, F16, F17, F18, F19, F20, F21, F22, F23, F24, F25, Print_Screen, Np_Decimal
