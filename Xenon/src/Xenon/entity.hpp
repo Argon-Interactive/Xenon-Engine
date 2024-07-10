42ccbe234d4fe82189e0e4e04e66434a84617a05
GoscZnickiem <maciek.6098@gmail.com>
2024-07-10 02:06:29 +0000

    sory przez przypadek tutaj zrobiłem rzeczy lol

---
+++ Xenon/src/Xenon/entity.hpp
@@ -0,0 +1,30 @@
+#ifndef _XENON_XENON_ENTITY_
+#define _XENON_XENON_ENTITY_
+
+#include "api.h"
+
+#include <cstdint>
+
+namespace Xenon {
+
+class XAPI Entity {
+public:
+	Entity();
+
+	//addComponent();
+	
+	//removeComponent();
+	
+	//getComponent();
+	
+private:
+	uint64_t m_uuid;
+
+
+};
+
+
+
+}
+
+#endif // !_XENON_XENON_ENTITY_
