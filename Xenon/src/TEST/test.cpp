5f1ea246b663cf1f5c77ef0201f0d42690dc4b61
GoscZnickiem <maciek.6098@gmail.com>
2024-08-15 20:31:31 +0200

    no implicit conversions in tests

--- Xenon/src/TEST/test.cpp
+++ Xenon/src/TEST/test.cpp
@@ -8,24 +8,24 @@ void Core::Test::test() {
 	ComponentCluster cl2;
 
 	
-	cl1.intComp.emplaceComponent(0, 1, cl1.floatComp.emplaceComponent(0, 1.5));
-	cl1.intComp.emplaceComponent(1, 2, cl1.floatComp.emplaceComponent(1, 2.5));
-	cl1.intComp.emplaceComponent(2, 3, cl1.floatComp.emplaceComponent(2, 3.5));
-	cl2.intComp.emplaceComponent(3, 4, cl2.floatComp.emplaceComponent(3, 4.5));
-	cl1.intComp.emplaceComponent(4, 5, cl1.floatComp.emplaceComponent(4, 5.5));
-	cl1.intComp.emplaceComponent(5, 6, cl1.floatComp.emplaceComponent(5, 6.5));
-	cl2.intComp.emplaceComponent(6, 7, cl2.floatComp.emplaceComponent(6, 7.5));
-	cl2.intComp.emplaceComponent(7, 8, cl2.floatComp.emplaceComponent(7, 8.5));
-	cl2.intComp.emplaceComponent(8, 9, cl2.floatComp.emplaceComponent(8, 9.5));
-	cl2.intComp.emplaceComponent(9, 10, cl2.floatComp.emplaceComponent(9, 10.5));
-	cl1.intComp.emplaceComponent(10, 11, cl1.floatComp.emplaceComponent(10, 11.5));
-	cl1.intComp.emplaceComponent(11, 12, cl1.floatComp.emplaceComponent(11, 12.5));
-	cl1.intComp.emplaceComponent(12, 13, cl1.floatComp.emplaceComponent(12, 13.5));
-	cl1.intComp.emplaceComponent(13, 14, cl1.floatComp.emplaceComponent(13, 14.5));
-	cl2.intComp.emplaceComponent(14, 15, cl2.floatComp.emplaceComponent(14, 15.5));
-	cl2.intComp.emplaceComponent(15, 16, cl2.floatComp.emplaceComponent(15, 16.5));
-	cl2.intComp.emplaceComponent(16, 17, cl2.floatComp.emplaceComponent(16, 17.5));
-	cl2.intComp.emplaceComponent(17, 18, cl2.floatComp.emplaceComponent(17, 18.5));
+	cl1.intComp.emplaceComponent(0, 1, cl1.floatComp.emplaceComponent(0, 1.5f));
+	cl1.intComp.emplaceComponent(1, 2, cl1.floatComp.emplaceComponent(1, 2.5f));
+	cl1.intComp.emplaceComponent(2, 3, cl1.floatComp.emplaceComponent(2, 3.5f));
+	cl2.intComp.emplaceComponent(3, 4, cl2.floatComp.emplaceComponent(3, 4.5f));
+	cl1.intComp.emplaceComponent(4, 5, cl1.floatComp.emplaceComponent(4, 5.5f));
+	cl1.intComp.emplaceComponent(5, 6, cl1.floatComp.emplaceComponent(5, 6.5f));
+	cl2.intComp.emplaceComponent(6, 7, cl2.floatComp.emplaceComponent(6, 7.5f));
+	cl2.intComp.emplaceComponent(7, 8, cl2.floatComp.emplaceComponent(7, 8.5f));
+	cl2.intComp.emplaceComponent(8, 9, cl2.floatComp.emplaceComponent(8, 9.5f));
+	cl2.intComp.emplaceComponent(9, 10, cl2.floatComp.emplaceComponent(9, 10.5f));
+	cl1.intComp.emplaceComponent(10, 11, cl1.floatComp.emplaceComponent(10, 11.5f));
+	cl1.intComp.emplaceComponent(11, 12, cl1.floatComp.emplaceComponent(11, 12.5f));
+	cl1.intComp.emplaceComponent(12, 13, cl1.floatComp.emplaceComponent(12, 13.5f));
+	cl1.intComp.emplaceComponent(13, 14, cl1.floatComp.emplaceComponent(13, 14.5f));
+	cl2.intComp.emplaceComponent(14, 15, cl2.floatComp.emplaceComponent(14, 15.5f));
+	cl2.intComp.emplaceComponent(15, 16, cl2.floatComp.emplaceComponent(15, 16.5f));
+	cl2.intComp.emplaceComponent(16, 17, cl2.floatComp.emplaceComponent(16, 17.5f));
+	cl2.intComp.emplaceComponent(17, 18, cl2.floatComp.emplaceComponent(17, 18.5f));
 
 	cl1.load();
 	cl2.load();
@@ -41,7 +41,7 @@ void Core::Test::test() {
 	cl1.floatComp.removeComponent(5);
 	cl2.intComp.removeComponent(14);
 	cl2.floatComp.removeComponent(14);
-	cl1.floatComp.addComponent(19, 20.5);
+	cl1.floatComp.addComponent(19, 20.5f);
 	cl1.intComp.addComponent(19, {20, nullptr});
 	cl1.syncComponentData();
 	cl2.syncComponentData();
@@ -50,7 +50,7 @@ void Core::Test::test() {
 		XN_LOG_INF("int: {0}, float: {0}", a.val, *a.ref);
 	}
 	XN_LOG_DEB("print succesfull");
-	cl2.floatComp.addComponent(5, 6.5);
+	cl2.floatComp.addComponent(5, 6.5f);
 	cl2.intComp.addComponent(5, {6, nullptr});
 	cl1.syncComponentData();
 	cl2.syncComponentData();
