8e4759eafd6b114f9bb45c3311c973c1a6e316a1
Oskar Meler <oskar@meler.pl>
2024-08-18 14:50:05 +0200

    deleted old code that was used for testing

--- Xenon/include/xenon.h
+++ Xenon/include/xenon.h
@@ -4,9 +4,6 @@
 #include"../src/Xenon/entryPoint.hpp"
 #include"../src/core/input/input.hpp"
 #include"Xenon/logger_client.hpp" 
-//TEST
-#include"TEST/test.hpp" 
-//!TEST
 
 
 #endif // !_XENON_XENON_H_
--- Xenon/src/TEST/test.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef _XN_TEMP_TEST_
-#define _XN_TEMP_TEST_
-#include "Xenon/api.h"
-namespace Core::Test { XAPI void test(); }
-#endif // !_XN_TEMP_TEST_
-
