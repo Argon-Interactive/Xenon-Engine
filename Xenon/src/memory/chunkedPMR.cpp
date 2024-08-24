744edfb62528c2e6ce3c945eba5179ebe3a08511
frogrammer9 <oskar@meler.pl>
2024-08-24 21:28:40 +0200

    deallocate was goofy so it had to be put down :(

--- Xenon/src/memory/chunkedPMR.cpp
+++ Xenon/src/memory/chunkedPMR.cpp
@@ -4,19 +4,18 @@
 
 Core::ChunkedPMR::ChunkedPMR(std::pmr::memory_resource* upstream) : m_upstream(upstream) {}
 
-Core::ChunkedPMR::~ChunkedPMR() { for(void* ptr : m_dealocationList) m_upstream->deallocate(ptr, s_CHUNK_SIZE, s_CHUNK_SIZE); }
+Core::ChunkedPMR::~ChunkedPMR() { for(void* ptr : m_dealocationList) m_upstream->deallocate(ptr, s_CHUNK_SIZE); }
 
 void* Core::ChunkedPMR::do_allocate(std::size_t bytes, std::size_t alignment) { 
 	assert(bytes % s_CHUNK_SIZE == 0 && "Memory allocation in ChunkedPMR must be a multiple of 4096 bytes");
 	void* ptr = nullptr;
 	const std::lock_guard<std::mutex> lock(m_mutex);
-	if(bytes == 1 && !m_chunkPool.empty()) {
+	if(bytes == s_CHUNK_SIZE && !m_chunkPool.empty()) {
 		ptr = m_chunkPool.front();
 		m_chunkPool.pop_front();
 	}
 	else {
 		ptr = m_upstream->allocate(bytes, alignment);
-		if(ptr == nullptr) throw std::bad_alloc();
 		m_dealocationList.push_back(ptr);
 	}
 	return ptr;
