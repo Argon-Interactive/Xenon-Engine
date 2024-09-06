0ee03fceb3488dbe3b8100879a134811a4bc5ff2
GoscZnickiem <maciek.6098@gmail.com>
2024-09-06 03:12:42 +0200

    Added ChunkedArray tests. Fixed a bug where ChunkedArray would not call destructor at pop_back()

---
+++ Tests/chunkedArray.cpp
@@ -0,0 +1,58 @@
+#include <catch2/catch_all.hpp>
+
+#include "ECS/chunkedArray.hpp"
+
+bool calledCon = false;
+bool calledDes = false;
+bool calledCop = false;
+bool calledMov = false;
+bool calledCAs = false;
+bool calledMAs = false;
+
+struct Test {
+	Test() { calledCon = true; }
+	~Test() { calledDes = true; }
+
+	Test(const Test & /*unused*/) { calledCop = true; }
+	Test(Test && /*unused*/) noexcept { calledMov = true; }
+
+	Test &operator=(const Test& other) { 
+		if(this == &other) return *this;
+		calledCAs = true; 
+		return *this;
+	}
+	Test &operator=(Test&& other) noexcept { 
+		if(this == &other) return *this;
+		calledMAs = true; 
+		return *this;
+	}
+};
+
+TEST_CASE("ChunkedArray", "[ECS][ChunkedArray]") {
+	Core::ChunkedArray<Test> ca;
+
+	SECTION("Adding") {
+		auto _ = GENERATE(range(1, 40));
+		ca.emplace_back();
+		REQUIRE(calledCon == true);
+		calledCon = false;
+		REQUIRE(calledDes == false);
+		REQUIRE(calledCop == false);
+		REQUIRE(calledMov == false);
+		REQUIRE(calledCAs == false);
+		REQUIRE(calledMAs == false);
+	}
+
+	SECTION("Deleting from back") {
+		auto _ = GENERATE(range(1, 40));
+
+		ca.pop_back();
+		REQUIRE(calledDes == true);
+		calledDes = false;
+		REQUIRE(calledCon == false);
+		REQUIRE(calledCop == false);
+		REQUIRE(calledMov == false);
+		REQUIRE(calledCAs == false);
+		REQUIRE(calledMAs == false);
+	}
+}
--- Tests/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-#include "ECS/entity.hpp"
-
-TEST_CASE( "test", "[test]" ) {
-	Core::Entity a = 13840910;
-    REQUIRE( a == 17 );
-}
